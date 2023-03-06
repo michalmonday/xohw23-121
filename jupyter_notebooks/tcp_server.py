7b6a7ce3276daec855d93e7a979f73f9584e4d82
michalmonday <michalmonday17@gmail.com>
2023-03-06 03:54:09 +0000

    tcp responder, basic protocol: list_programs, runs_program

--- jupyter_notebooks/tcp_server.py
+++ jupyter_notebooks/tcp_server.py
@@ -3,8 +3,15 @@ import threading
 import time
 import queue
 import random
+import json
+from pathlib import Path
 
 SEND_TO_ALL = -1
+
+# Each directory within PROGRAMS_DIR will contain a group of programs
+# each group of programs will have a baseline program and a set of variations
+# that are anomalous in some way. 
+PROGRAMS_DIR = Path('/home/xilinx/programs')
 
 def get_my_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -37,11 +44,12 @@ class Client(threading.Thread):
     def run(self):
         while True:
             try:
-                data = self.socket.recv(1024)
+                data = self.socket.recv(4096)
                 # 2 lines below are just for testing
-                data_str = data.decode("utf-8")
+                msgs = [msg for msg in data.decode("utf-8").splitlines() if msg]
                 # to_send_queue.put((self.id, data_str))
-                self.tcp_server_received_queue.put((self.id, data_str))
+                for msg in msgs:
+                    self.tcp_server_received_queue.put((self.id, msg))
             except:
                 print("Client " + str(self.address) + " has disconnected")
                 self.signal = False
@@ -62,6 +70,11 @@ class Client(threading.Thread):
                 #         print("Sent to " + str(client))
 
 class TCP_Server:
+    ''' TCP Server that can be used to send/receive data from multiple clients.
+        It spawns 3 threads:
+         - listen_worker: listens for new connections and creates a new Client object for each one
+         - send_worker: sends data to clients (from the to_send_queue)
+         - respond_worker: receives data from clients and responds to them '''
     def __init__(self, host_ip='0.0.0.0', port=9092):
         self.host_ip = host_ip
         self.port = port
@@ -73,6 +86,7 @@ class TCP_Server:
         # Maybe it will be able to get a list of available programs (all files in dir) and select one.
         self.received_queue = queue.Queue()
         self.to_send_queue = queue.Queue()
+        self.end_queue = queue.Queue()
         self.total_connections = 0
 
         #Create new server socket
@@ -82,9 +96,30 @@ class TCP_Server:
         listen_sock.listen(2)
         self.listen_thread = threading.Thread(target=self.listen_worker, args=(listen_sock,), daemon=True)
         self.send_thread = threading.Thread(target=self.send_worker, daemon=True)
-        self.listen_thread.start()
+
+        self.protocol = {} # key = received_message, value = function to call that returns a string response message and/or does some action
+        # respond_thread checks if received messages match protocol and responds accordingly
+        self.respond_thread = threading.Thread(target=self.respond_worker, daemon=True)
+        
+    def start(self):
+        self.respond_thread.start()
         self.send_thread.start()
-        self.end_queue = queue.Queue()
+        self.listen_thread.start()
+
+
+    def register_protocol_message(self, received_message, response_function, received_message_format=''):
+        '''Registering a protocol message means that when the server receives specified message,
+        it will call the response_function (which must return a string response message). Response 
+        message should probably be a json string to make parsing easier. Registered functions
+        must take care of locking/unlocking data structures that are used in other threads. '''
+        if not isinstance(received_message, str):
+            raise Exception('received_message must be a string')
+        if not callable(response_function):
+            raise Exception('response_function must be a function')
+        if received_message in self.protocol:
+            print(f'received_message ("{received_message}") was already registered, overwriting it.')
+        self.protocol[received_message] = response_function
+        
 
     def data_available(self):
         return not self.received_queue.empty()
@@ -102,6 +137,8 @@ class TCP_Server:
         self.to_send_queue.put({'client_id': SEND_TO_ALL, 'data': data})
 
     def send_to_client(self, client_id, data):
+        if not data.endswith('\n'):
+            data += '\n'
         self.to_send_queue.put({'client_id': client_id, 'data': data})
 
     def send(self, data):
@@ -140,6 +177,22 @@ class TCP_Server:
             client.start()
             total_connections += 1
         
+    def respond_worker(self):
+        while True:
+            if not self.data_available():
+                time.sleep(0.01)
+                continue
+            for client_id, data_str in self.read_data(): 
+                print(f'client_id={client_id}, data_str={data_str}')
+                print(f'protocol.keys()={self.protocol.keys()}')
+                if any(data_str.startswith(s) for s in self.protocol):
+                    tokens = data_str.split(',')
+                    func = self.protocol[tokens[0]]
+                    protocol_entry = tokens[0]
+                    print(f'Calling {func} for the "{protocol_entry}" with args={tokens[1:]}')
+                    response = self.protocol[protocol_entry](*tokens[1:])
+                    print(f'response={response}')
+                    self.send_to_client(client_id, response)
     
     def end(self):
         self.end_queue.put(True)
