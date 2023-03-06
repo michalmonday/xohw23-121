fd586cff6bfc6f516f44b3f9f27fa0603748a2da
michalmonday <michalmonday17@gmail.com>
2023-03-06 03:54:53 +0000

    Capacitive touch (TAMC_GT911 lib with a workaround)

--- ESP32-3248S035C_display_board/src/ESP32-3248S035C_display_board.ino
+++ ESP32-3248S035C_display_board/src/ESP32-3248S035C_display_board.ino
@@ -3,6 +3,8 @@
 #include <SPI.h>
 #include <WiFi.h>
 #include <map>
+#include <cJSON.h>
+#include <TAMC_GT911.h>
 
 // #include "drawing.h"
 #include "colours.h"
@@ -42,6 +44,14 @@ WiFiClient client;
 // display object
 TFT_eSPI tft = TFT_eSPI(); 
 
+#define TOUCH_SDA  33
+#define TOUCH_SCL  32
+#define TOUCH_INT 36
+#define TOUCH_RST 25
+#define TOUCH_WIDTH  480
+#define TOUCH_HEIGHT 320
+TAMC_GT911 *touch; 
+
 // status display is a GUI component that covers bottom of the screen and can display status messages like "Connecting to WiFi"
 StatusDisplay status_display(tft, RESOLUTION_X, (int)(0.1 * RESOLUTION_Y), 0, (0.9*RESOLUTION_Y), TFT_WHITE, TFT_BLACK);
 
@@ -63,9 +73,7 @@ int max_number_of_items = min((int)(xhi - xlo), (int)graph.get_width());
 void init_wifi() {
     tft.setTextColor(WHITE);
     //tft.setTextSize(2);
-    char buf[100];
-    sprintf(buf, "Connecting to '%s' WiFi access point...", ACCESS_POINT_SSID);
-    status_display.set_status("ap_connection_status", buf);
+    status_display.set_status("ap_connection_status", "Connecting to '" + String(ACCESS_POINT_SSID) + "' WiFi access point...");
     status_display.set_status("tcp_connection_status", "ZYNQ TCP server address is set to: " + server_ip_str);
 
     WiFi.mode(WIFI_STA);
@@ -80,47 +88,17 @@ void init_wifi() {
     Serial.println("Connected");
     Serial.print("IP Address:");
     Serial.println(WiFi.localIP());
-    sprintf(buf, "Connected to '%s' access point (assigned IP: %s)", ACCESS_POINT_SSID, WiFi.localIP().toString().c_str());
-    status_display.set_status("ap_connection_status", buf);
+    status_display.set_status("ap_connection_status", "Connected to '" + String(ACCESS_POINT_SSID) + "' WiFi access point (assigned IP: " + WiFi.localIP().toString() + ")");
 }
 
-LinePlot* create_new_line_plot() {
-    unsigned int clr = colours[current_colour_id];
-    current_colour_id = (current_colour_id + 1) % (sizeof(colours) / sizeof(colours[0]));
-    Serial.printf("current_colour_id=%d\n", current_colour_id);
-    return new LinePlot(tft, xlo, xhi, ylo, yhi, clr, max_number_of_items);
-}
+// LinePlot* create_new_line_plot() {
+//     unsigned int clr = colours[current_colour_id];
+//     current_colour_id = (current_colour_id + 1) % (sizeof(colours) / sizeof(colours[0]));
+//     Serial.printf("current_colour_id=%d\n", current_colour_id);
+//     return new LinePlot(tft, xlo, xhi, ylo, yhi, clr, max_number_of_items);
+// }
 
 
-void check_protocol(String line) {
-    // line is a string received from the ZC706 tcp server (currently using PYNQ)
-
-    if (line.startsWith("create_plot")) {
-        char plot_name[20];
-        sscanf(line.c_str(), "create_plot:%s", plot_name);
-        LinePlot *line_plot = new LinePlot(tft, xlo, xhi, ylo, yhi, CYAN, max_number_of_items);
-        if (graph.add_plot(String(plot_name), line_plot)) {
-            client.write("OK");
-        } else {
-            client.write("ERROR: Plot already exists.");
-        }
-    }
-
-    if (line.startsWith("add_point")) {
-        char plot_name[20];
-        double value;
-        sscanf(line.c_str(), "add_point:%[^,],%lf", plot_name, &value);
-        LinePlot* line_plot = graph.get_plot(plot_name);
-        if (!line_plot) {
-            Serial.printf("add_point was used but plot %s does not exist. Creating it now.\n", plot_name);
-            line_plot = graph.add_plot(String(plot_name), create_new_line_plot());
-
-        }
-        line_plot->draw(BLACK);
-        line_plot->add_point(value);
-        line_plot->draw();
-    }
-}
 
 void drawing_thread_func(void *parameter) {
     while (true) {
@@ -142,16 +120,20 @@ void init_display() {
     tft.fillScreen(BLACK);
     // tft.setRotation(1); // landscape
     tft.setRotation(3); // landscape, upside downh
-    tft.fillScreen(BLACK);
 }
 
 void setup() {
+    touch = new TAMC_GT911(TOUCH_SDA, TOUCH_SCL, TOUCH_INT, TOUCH_RST, TOUCH_WIDTH, TOUCH_HEIGHT);
+    touch->reset();
+    touch->begin();
+    touch->setRotation(ROTATION_LEFT);
+    delay(100);
+
     Serial.begin(115200);
     Serial.println();
     serial_riscv.begin(115200);
     init_display();
     init_wifi();
-
 
     static bool redraw_on_first_call_only_graph = true;
     delay(1000);
@@ -190,13 +172,6 @@ void swap(char &a, char &b) {
     b = t;
 }
 
-String reverse(String str) {
-    int n = str.length();
-    for (int i = 0; i < n / 2; i++)
-        swap(str[i], str[n - i - 1]);
-    return str;
-}
-
 bool add_string_to_queue(String *str) {
     if (xQueueSendToBack(queue, &str, 0) == errQUEUE_FULL) {
         // Serial.println("Queue is full, ignoring...");
@@ -209,6 +184,15 @@ bool add_string_to_queue(String *str) {
     return true;
 }
 
+bool contains_digits_only(String str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (!isdigit(str[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 void handle_riscv_serial() {
     while (serial_riscv.available()) {
         // Serial.println("Received something from riscv");
@@ -217,11 +201,17 @@ void handle_riscv_serial() {
         // Serial.print(line);
         // Serial.println("'");
 
-        float ecg_value = atof(reverse(line).c_str()) / 60000.0f;
+        if (!contains_digits_only(line)) {
+            Serial.println(line);
+            Serial.println("Not a number");
+            continue;
+        }
+
+        float ecg_value = atof(line.c_str()) / 60000.0f;
         // Serial.print("ecg_value=");
         // Serial.println(ecg_value);
 
-        if (ecg_value < 0.05 || ecg_value > 1.0) {
+        if (ecg_value < 0.0 || ecg_value > 1.0) {
             Serial.println(line);
             Serial.print("ecg_value=");
             Serial.println(ecg_value);
@@ -237,8 +227,86 @@ void handle_riscv_serial() {
     }
 }
 
+//void check_protocol(String line) {
+//    // line is a string received from the ZC706 tcp server (currently using PYNQ)
+//    if (line.startsWith("create_plot")) {
+//        char plot_name[20];
+//        sscanf(line.c_str(), "create_plot:%s", plot_name);
+//        LinePlot *line_plot = new LinePlot(tft, xlo, xhi, ylo, yhi, CYAN, max_number_of_items);
+//        if (graph.add_plot(String(plot_name), line_plot)) {
+//            client.write("OK");
+//        } else {
+//            client.write("ERROR: Plot already exists.");
+//        }
+//    }
+//
+//    if (line.startsWith("add_point")) {
+//        char plot_name[20];
+//        double value;
+//        sscanf(line.c_str(), "add_point:%[^,],%lf", plot_name, &value);
+//        LinePlot* line_plot = graph.get_plot(plot_name);
+//        if (!line_plot) {
+//            Serial.printf("add_point was used but plot %s does not exist. Creating it now.\n", plot_name);
+//            line_plot = graph.add_plot(String(plot_name), create_new_line_plot());
+//
+//        }
+//        line_plot->draw(BLACK);
+//        line_plot->add_point(value);
+//        line_plot->draw();
+//    }
+//}
+
+void check_protocol(String line) {
+    cJSON *root = cJSON_Parse(line.c_str());
+    // Parse the following json string:
+    // '{"ECG": ["ecg_info_leak.bin", "ecg_baseline.bin"], "ANOTHER": ["another0.bin", "another1.bin"]}'
+    if (root == NULL) {
+        Serial.println("Failed to parse json");
+        return;
+    }
+    // check if ECG is present
+
+    if (cJSON_HasObjectItem(root, "programs")) {
+        cJSON *programs = cJSON_GetObjectItem(root, "programs");
+        cJSON *program_category = programs->child;
+        while(program_category) {
+            Serial.println(program_category->string);
+            for (int i=0; i<cJSON_GetArraySize(program_category); i++) {
+                cJSON *program = cJSON_GetArrayItem(program_category, i);
+                Serial.print("Program: ");
+                Serial.println(program->valuestring);
+            }
+            program_category = program_category->next;
+        }
+    }
+
+    if (cJSON_HasObjectItem(root, "status_update")) {
+        cJSON *status_update = cJSON_GetObjectItem(root, "status_update");
+        // Print response
+        Serial.print("Status update: ");
+        Serial.println(status_update->valuestring);
+    }
+
+    cJSON_Delete(root);
+}
+
+void handle_touch() {
+    touch->read();
+    if (touch->isTouched){
+        Serial.printf("Touch: x=%d, y=%d\n", touch->points[0].x, touch->points[0].y);
+        // for (int i=0; i<touch->touches; i++){
+        //     Serial.print("Touch ");Serial.print(i+1);Serial.print(": ");;
+        //     Serial.print("  x: ");Serial.print(touch->points[i].x);
+        //     Serial.print("  y: ");Serial.print(touch->points[i].y);
+        //     Serial.print("  size: ");Serial.println(touch->points[i].size);
+        //     Serial.println(' ');
+        // }
+    }
+}
+
 void loop(void) {
     handle_riscv_serial();
+    handle_touch();
     static bool redraw_on_first_call_only_trace4 = true;
 
     Serial.println("Attempt to access server...");
@@ -248,6 +316,7 @@ void loop(void) {
         client.stop();
         for (int i=5; i>0; i--) {
             handle_riscv_serial();
+            handle_touch();
             // status_display.set_status("tcp_connection_status", "Retrying ZC706 TCP server (" + server_ip_str + ") connection in " + String(i) + " seconds...");
             delay(1000);
         }
@@ -256,17 +325,22 @@ void loop(void) {
     // status_display.set_status("tcp_connection_status", "Connected to ZC706 TCP server ("+  server_ip_str + ")");
 
     Serial.println("Access successful.");
-    client.print("Hello world!");
+    client.println("list_programs");
+    client.println("run_program,ecg_baseline.bin");
+
+    // client.print("run_program:ecg_baseline.bin");
     while (client.connected() || client.available()) {
         handle_riscv_serial();
+        handle_touch();
 
         while (client.available()) {
-            String *line = new String(client.readStringUntil('\n'));
+            // String *line = new String(client.readStringUntil('\n'));
+            String line = client.readStringUntil('\n');
             // String line = client.readString(); // readString has a timeout that would make it inefficient (readStringUntil does not have it when using '\n')
             Serial.print("Received: '");
-            Serial.print(*line);
+            Serial.print(line);
             Serial.println("'");
-            add_string_to_queue(line);
+            check_protocol(line);
         }
     }
     Serial.println("Closing connection.");
